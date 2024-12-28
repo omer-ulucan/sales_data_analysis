bf52e6fa0d5d6217c01dd71e2fc88fff0e5fdab3
Ömer Ulucan <omer.ulucan80@gmail.com>
2024-12-27 22:28:46 -0600

    sales.py fixed

--- apis/sales.py
+++ apis/sales.py
@@ -1,4 +1,6 @@
 from fastapi import APIRouter, HTTPException
+import pandas as pd
+from database.database import get_connection
 
 router = APIRouter()
 
@@ -8,4 +10,7 @@ async def test_api():
         "message": "Hello World!"
     }
     
-@router.get("/sales-prices")
+@router.get("/total-revenue")
+async def total_revenue():
+    with get_connection() as connection:
+        df = pd.read_sql("SELECT * FROM sales", connection)
